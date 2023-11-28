4fd45850b6f31d615a615f1ebb11d55cd49e2622
root <root@AYOMIDE>
2023-11-28 13:58:15 +0100

    added a function to connect to the access database and write the content of the wpi data to a csv file

--- worldport.py
+++ worldport.py
@@ -24,3 +24,29 @@ def download_zip_file(url, destination_folder):
 zip_url = "https://drive.google.com/uc?export=download&id=1VyCGCAfFuEK7vB1C9Vq8iPdgBdu-LDM4"
 project_folder = r"C:\Users\HP\OneDrive\Documents\Worldport_data"
 download_zip_file(zip_url, project_folder)
+
+def read_wpi_data(access_file_path, table_name):
+    conn_str = f"DRIVER={{Microsoft Access Driver (*.mdb, *.accdb)}};DBQ={access_file_path}"
+    conn = pyodbc.connect(conn_str)
+    cursor = conn.cursor()
+    # Query to select all data from the "Wpi Data" table with double quotes around the table name
+    query = f'SELECT * FROM "{table_name}"'
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df
+mdb_file_path = os.path.join(project_folder, "WPI.mdb")
+table_name = "Wpi Data"
+wpi_data = read_wpi_data(mdb_file_path, table_name)
+wpi_data.columns = wpi_data.columns.str.lower() 
+wpi_data.to_csv('raw/worldport.csv', index=False)
+print('worldport data written successfully to csv file')
+
+
+
+
+
+
+
+
+
+
